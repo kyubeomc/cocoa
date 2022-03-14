3df5ee61103325acb4a7a007d39d41a6db87d7f9
kyubeomc <96153712+kyubeomc@users.noreply.github.com>
2022-03-14 23:24:16 +0900

    0324

---
+++ Python/practice0314.py
@@ -0,0 +1,27 @@
+a_string = "like this"
+a_number = 3
+
+a_boolean = False
+
+super_long_variable = False
+print(super_long_variable)
+
+day = ["Mon", "Tue","Wen","Thur","Fri"]
+
+print("Mon" in day)
+
+day.append("Thomas")
+print(day)
+
+name = "Kyu"
+age = 27
+korean = True
+fav_food
+
+thomas = {
+    "name" : "Nico",
+    "age" : 27,
+    "Korean" : True,
+    "fav_"
+}
+
